85aa5d9f4bc6e6b6e7c3cfa51d1993ee85c62001
Ryoichi KATO <ryo1kato@gmail.com>
2016-09-30 23:15:00 +0000

    Using config.mk for uml_{net,router,switch}

--- config.mk
+++ config.mk
@@ -1,28 +1,38 @@
 # Makefile configurations common for the subdirectories
 
-BIN_DIR = /usr/bin
-SBIN_DIR = /usr/sbin
+BIN_DIR  := /usr/bin
+SBIN_DIR := /usr/sbin
 
 ifeq ($(shell uname -m),x86_64)
-LIB_DIR = /usr/lib64/uml
+LIB_DIR  := /usr/lib64/uml
 else
-LIB_DIR = /usr/lib/uml
+LIB_DIR  := /usr/lib/uml
 endif
 
-CFLAGS = -g -Wall
-LDFLAGS =
+CFLAGS   := -g -Wall
+LDFLAGS  :=
 export BIN_DIR LIB_DIR CFLAGS LDFLAGS
 
+TUNTAP := $(shell [ -e /usr/include/linux/if_tun.h ] && echo -DTUNTAP)
 
+
+#############################################################################
+##
+## Only for subdirectories
+##
 ifndef TOPLEVELMAKEFILE
 
+override CFLAGS += $(TUNTAP)
 SRCS ?= $(wildcard *.c)
-OBJS ?= $(SRCS:.c=.o)
+ifeq ($(TUNTAP),)
+SRCS := $(filter-out $(SRCS_TUNTAP),$(SRCS))
+endif
+OBJS ?= $(sort $(SRCS:.c=.o))
 
 all:: $(BIN) $(LIB)
 
 $(BIN): $(OBJS)
-	$(CC) $(CFLAGS) -o $(BIN) $(OBJS) $(LIBS) $(LDFLAGS)
+	$(CC) $(CFLAGS) -o $@ $^ $(LIBS) $(LDFLAGS)
 
 $(LIB): $(OBJS)
 	$(AR) r $@ $<
@@ -33,11 +43,12 @@ clean::
 install:: $(BIN) $(LIB)
 ifndef SKIP_INSTALL
 ifneq ($(BIN),)
-	install -D -s $(BIN) $(DESTDIR)$(BIN_DIR)/$(BIN)
+	install -D $(INSTALL_OPTS_$(BIN)) -s $(BIN) $(DESTDIR)$(BIN_DIR)/$(BIN)
 endif
 ifneq ($(LIB),)
-	install -D -s $(LIB) $(DESTDIR)$(LIB_DIR)/$(LIB)
+	install -D $(INSTALL_OPTS_$(LIB)) -s $(LIB) $(DESTDIR)$(LIB_DIR)/$(LIB)
 endif
 endif #SKIP_INSTALL
+
 
 endif #TOPLEVELMAKEFILE
